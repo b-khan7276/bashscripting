0b1bfbe46c721a622ca8833620e76ad8376661cf
Muhammad Bilal Khan <58091942+b-khan7276@users.noreply.github.com>
2020-10-02 09:17:08 -0700

    Create helloword.sh

---
+++ helloword.sh
@@ -0,0 +1,26 @@
+to check shells in your linux machine type command 
+        => cat /ect/shells
+To to check your bash shell:
+        => which shell
+To creat a file:
+      => touch helloworld.sh 
+      
+    Hello bash  Program 
+******************* Hello bash  Program ********************
+          helloworld.sh
+#! /usr/bin/bash
+echo "Hello world"
+
+*************************************************************
+
+To run  the script you have to first give the permission 
+
+in order to see the permission:
+          => ls -al
+To give the permission to run script:
+      => chmod +x hellobash.sh 
+now run:
+  = > ./hellobash.sh 
+  
+******************* Redirecting text  ********************
+
